3fe162ba6494aa4b99eebe8f844369eb41a1fb5c
inksp0ts09 <maryanne.mcglone09@gmail.com>
2022-05-26 09:35:19 -0400

    Initial commit

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import SheCodesLogo from "./SheCodesLogo.png";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={SheCodesLogo} className="App-logo, img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
